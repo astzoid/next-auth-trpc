2f8706b83962047466add08bef9cd1f0230081a3
enum314 <enum314@gmail.com>
2022-12-20 23:05:00 +0800

    feat: next-auth

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,6 +1,30 @@
-import { initTRPC } from '@trpc/server';
+import { initTRPC, TRPCError } from '@trpc/server';
 
-const t = initTRPC.create();
+import type { Context } from './context';
+
+const t = initTRPC.context<Context>().create({});
 
 export const router = t.router;
 export const procedure = t.procedure;
+
+/**
+ * Reusable middleware to ensure
+ * users are logged in
+ */
+const isAuthed = t.middleware(({ ctx, next }) => {
+	if (!ctx.session || !ctx.session.user) {
+		throw new TRPCError({ code: 'UNAUTHORIZED' });
+	}
+
+	return next({
+		ctx: {
+			// infers the `session` as non-nullable
+			session: { ...ctx.session, user: ctx.session.user },
+		},
+	});
+});
+
+/**
+ * Auth procedure
+ **/
+export const authProcedure = t.procedure.use(isAuthed);
